f2f12d95c983e20dc1090f12b77e78214926b780
Jiaming <wjmbjut@126.com>
2021-06-29 17:10:31 +0800

    第一次提交

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,14 @@
+﻿using Mirror;
+using UnityEngine;
+
+namespace QuickStart
+{
+    public class PlayerScript : NetworkBehaviour
+    {
+        public override void OnStartLocalPlayer()
+        {
+            transform.SetParent(Camera.main.transform);
+            transform.localPosition = new Vector3(0, 0, 0);
+        }
+    }
+}
